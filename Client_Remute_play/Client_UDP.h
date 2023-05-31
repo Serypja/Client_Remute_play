b238d51f858593f922743ff8f742d03849c5df05
Mirkil S <Mirkil555222@gmail.com>
2023-05-31 08:44:50 +0300

    Добавьте файлы проекта.

---
+++ Client_Remute_play/Client_UDP.h
@@ -0,0 +1,15 @@
+#pragma once
+#include"MyForm.h"
+#include<windows.h>
+#include<iostream>
+
+using namespace System::Windows::Forms;
+using namespace System::Net;
+using namespace System::Net::Sockets;
+using namespace System::Drawing;
+using namespace System::IO;
+
+void client_udp(PictureBox^ pictureBox1)
+{
+    
+}
---
+++ Client_Remute_play/MyForm.h
@@ -0,0 +1,121 @@
+#pragma once
+#include<windows.h>
+#include<iostream>
+
+using namespace System::Windows::Forms;
+using namespace System::Net;
+using namespace System::Net::Sockets;
+using namespace System::Drawing;
+using namespace System::IO;
+
+namespace ClientRemuteplay {
+
+	using namespace System;
+	using namespace System::ComponentModel;
+	using namespace System::Collections;
+	using namespace System::Windows::Forms;
+	using namespace System::Data;
+	using namespace System::Drawing;
+
+	/// <summary>
+	/// Сводка для MyForm
+	/// </summary>
+	public ref class MyForm : public System::Windows::Forms::Form
+	{
+	public:
+		MyForm(void)
+		{
+			InitializeComponent();
+			//
+			//TODO: добавьте код конструктора
+			//
+		}
+
+	protected:
+		/// <summary>
+		/// Освободить все используемые ресурсы.
+		/// </summary>
+		~MyForm()
+		{
+			if (components)
+			{
+				delete components;
+			}
+		}
+	private: System::Windows::Forms::Button^ button1;
+	private: System::Windows::Forms::PictureBox^ pictureBox1;
+	protected:
+
+	private:
+		/// <summary>
+		/// Обязательная переменная конструктора.
+		/// </summary>
+		System::ComponentModel::Container ^components;
+
+#pragma region Windows Form Designer generated code
+		/// <summary>
+		/// Требуемый метод для поддержки конструктора — не изменяйте 
+		/// содержимое этого метода с помощью редактора кода.
+		/// </summary>
+		void InitializeComponent(void)
+		{
+			this->button1 = (gcnew System::Windows::Forms::Button());
+			this->pictureBox1 = (gcnew System::Windows::Forms::PictureBox());
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->pictureBox1))->BeginInit();
+			this->SuspendLayout();
+			// 
+			// button1
+			// 
+			this->button1->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 16, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(204)));
+			this->button1->Location = System::Drawing::Point(12, 12);
+			this->button1->Name = L"button1";
+			this->button1->Size = System::Drawing::Size(194, 84);
+			this->button1->TabIndex = 0;
+			this->button1->Text = L"Подключиться UDP";
+			this->button1->UseVisualStyleBackColor = true;
+			this->button1->Click += gcnew System::EventHandler(this, &MyForm::button1_Click);
+			// 
+			// pictureBox1
+			// 
+			this->pictureBox1->Location = System::Drawing::Point(358, 12);
+			this->pictureBox1->Name = L"pictureBox1";
+			this->pictureBox1->Size = System::Drawing::Size(1408, 703);
+			this->pictureBox1->TabIndex = 1;
+			this->pictureBox1->TabStop = false;
+			// 
+			// MyForm
+			// 
+			this->AutoScaleDimensions = System::Drawing::SizeF(6, 13);
+			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
+			this->ClientSize = System::Drawing::Size(1778, 727);
+			this->Controls->Add(this->pictureBox1);
+			this->Controls->Add(this->button1);
+			this->Name = L"MyForm";
+			this->Text = L"MyForm";
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->pictureBox1))->EndInit();
+			this->ResumeLayout(false);
+
+		}
+#pragma endregion
+	private: System::Void button1_Click(System::Object^ sender, System::EventArgs^ e) {
+		// Создаем UDP клиент на порту 1234
+		UdpClient^ udpClient = gcnew UdpClient(1234);
+		udpClient->Client->Bind(gcnew IPEndPoint(IPAddress::Any, 0));
+
+		// Получаем массив байт от сервера
+		IPEndPoint^ remoteEP = gcnew IPEndPoint(IPAddress::Any, 0);
+		array<Byte>^ receivedBytes = udpClient->Receive(remoteEP);
+
+		// Создаем Bitmap из массива байт
+		MemoryStream^ stream = gcnew MemoryStream(receivedBytes);
+		Image^ image = Image::FromStream(stream);
+		Bitmap^ bitmap = gcnew Bitmap(image);
+
+		// Используем Bitmap по своему усмотрению
+		// ...
+		pictureBox1->Image = bitmap;
+		pictureBox1->SizeMode = PictureBoxSizeMode::Zoom;
+	}
+	};
+}
