369878a6499b530519e8c949cec3eb61aa955bb4
Mirkil S <Mirkil555222@gmail.com>
2023-06-06 04:32:16 +0700

    Передача изображений до 64 МБ

--- Client_Remute_play/MyForm.h
+++ Client_Remute_play/MyForm.h
@@ -1,12 +1,19 @@
 #pragma once
 #include<windows.h>
 #include<iostream>
+//#include <winsock2.h>
+#using <System.dll>
+#include <list>
+
+using namespace System::Text;
+using namespace System::Collections::Generic;
 
 using namespace System::Windows::Forms;
 using namespace System::Net;
 using namespace System::Net::Sockets;
 using namespace System::Drawing;
 using namespace System::IO;
+using namespace System::Threading::Tasks;
 
 namespace ClientRemuteplay {
 
@@ -44,6 +51,7 @@ namespace ClientRemuteplay {
 		}
 	private: System::Windows::Forms::Button^ button1;
 	private: System::Windows::Forms::PictureBox^ pictureBox1;
+	private: System::Windows::Forms::Label^ label1;
 	protected:
 
 	private:
@@ -61,6 +69,7 @@ namespace ClientRemuteplay {
 		{
 			this->button1 = (gcnew System::Windows::Forms::Button());
 			this->pictureBox1 = (gcnew System::Windows::Forms::PictureBox());
+			this->label1 = (gcnew System::Windows::Forms::Label());
 			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->pictureBox1))->BeginInit();
 			this->SuspendLayout();
 			// 
@@ -80,29 +89,86 @@ namespace ClientRemuteplay {
 			// 
 			this->pictureBox1->Location = System::Drawing::Point(358, 12);
 			this->pictureBox1->Name = L"pictureBox1";
-			this->pictureBox1->Size = System::Drawing::Size(1408, 703);
+			this->pictureBox1->Size = System::Drawing::Size(418, 375);
 			this->pictureBox1->TabIndex = 1;
 			this->pictureBox1->TabStop = false;
+			// 
+			// label1
+			// 
+			this->label1->AutoSize = true;
+			this->label1->Location = System::Drawing::Point(12, 331);
+			this->label1->Name = L"label1";
+			this->label1->Size = System::Drawing::Size(35, 13);
+			this->label1->TabIndex = 2;
+			this->label1->Text = L"label1";
 			// 
 			// MyForm
 			// 
 			this->AutoScaleDimensions = System::Drawing::SizeF(6, 13);
 			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
-			this->ClientSize = System::Drawing::Size(1778, 727);
+			this->ClientSize = System::Drawing::Size(788, 399);
+			this->Controls->Add(this->label1);
 			this->Controls->Add(this->pictureBox1);
 			this->Controls->Add(this->button1);
 			this->Name = L"MyForm";
 			this->Text = L"MyForm";
 			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->pictureBox1))->EndInit();
 			this->ResumeLayout(false);
+			this->PerformLayout();
 
 		}
 #pragma endregion
-	private: System::Void button1_Click(System::Object^ sender, System::EventArgs^ e) {
-
 		
+		private: System::Void button1_Click(System::Object^ sender, System::EventArgs^ e) {
 
 
-	}
+			// Создание объекта UdpClient для прослушивания входящих пакетов
+			UdpClient^ client = gcnew UdpClient(8884);
+			IPAddress^ serverAddress = IPAddress::Parse("192.168.0.149");
+			IPEndPoint^ endPoint = gcnew IPEndPoint(serverAddress, 8884);
+			int maxPacketSize = 65507; // максимальный размер пакета UDP
+
+			// Создание буфера для хранения принятых данных
+			List<Byte>^ receivedData = gcnew List<Byte>();
+			
+
+			// Прием пакетов и добавление данных в буфер
+			while (true)
+			{
+				array<Byte>^ packetData = client->Receive(endPoint);
+				receivedData->AddRange(packetData);
+
+				// Если приняты все пакеты, выход из цикла
+				if (packetData->Length < maxPacketSize)
+					break;
+			}
+
+			// Преобразование списка байтов в массив байтов
+			array<Byte>^ pngData = receivedData->ToArray();
+
+			// Сохранение принятых данных в файл
+			System::IO::File::WriteAllBytes("image.png", pngData);
+
+			
+			
+		
+		}
 	};
 }
+
+//______________________________________________Передача картинки до 64 МБ
+//// Создаем UDP сервер
+//UdpClient^ server = gcnew UdpClient(8884);
+
+//// Получаем адрес и порт отправителя
+//IPEndPoint^ ipaddres_poluch = gcnew IPEndPoint(IPAddress::Any, 0);
+
+//// Принимаем данные от клиента
+//array<Byte>^ data = server->Receive(ipaddres_poluch);
+
+//
+//// Сохраняем принятые данные в файл
+//System::IO::File::WriteAllBytes("image.png", data);
+
+//// Закрываем сервер
+//server->Close();
